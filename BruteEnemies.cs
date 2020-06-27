cd88c18b881b94decf86b177f38d7fdcedac92cf
Henrique Blay Barboza <38546106+TheBlay@users.noreply.github.com>
2020-06-26 21:51:35 -0300

    Uploaded mod files
    The first release files of the mod.

---
+++ BruteEnemies.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.Utilities;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace BruteEnemies
+{
+	public class BruteEnemies : Mod
+	{
+	}
+}
---
+++ GeneralMobOverride.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Utilities;
+using Microsoft.Xna.Framework;
+
+namespace BruteEnemies
+{
+    class GeneralMobOverride : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            base.SetDefaults(npc);
+            npc.knockBackResist = 0f;
+        }
+    }
+}
